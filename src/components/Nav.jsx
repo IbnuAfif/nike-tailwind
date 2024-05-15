e733d6a0fc36a52a6485b2c63fef17dee5fa37f8
Muhammad Ibnu Afif <88562045+IbnuAfif@users.noreply.github.com>
2024-05-15 20:13:26 +0700

    create-navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,27 @@
+import headerLogo from "../assets/images/header-logo.svg";
+import { navLinks } from "../constants";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className="padding-x py-8 absolute w-full z-10">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} width={130} height={29} alt="logo" />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 ">
+          {navLinks.map((item, i) => (
+            <li
+              key={i}
+              href={item.href}
+              className="font-montserrat leading-normal"
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
